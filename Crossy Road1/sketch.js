09a09c94118df35d5c4842fad40412faac9955ca
Nisha <nish.dec@gmail.com>
2021-07-06 20:09:48 +0530

    changes in sketch.js function draw

--- Crossy Road1/sketch.js
+++ Crossy Road1/sketch.js
@@ -14,40 +14,52 @@ function setup() {
   createCanvas(1366,2700);
   carGroup1 = new Group();
   logGroup1 = new Group();
-
-for(var i=0; i<6; i++) {
-  var bottomGrass1 = createSprite(683,height-50-(i*400),width,grassHeight)
-
-if(i%2===0){
-  var road = createSprite(683,height-150-(i*400)-grassHeight,width,300)
-  road.shapeColor="black"
-}
-bottomGrass1.shapeColor = "grey"
-}
-
-for(var i = 0; i<40; i++){
-  cars = new Car(2);
-  carGroup1.add(cars.spt)
-}
-
-for(i=1;i<logGroup1.length;i++){
-  if(logGroup1[b].x<0){
-    logGroup1[i].x=width
+  
+  //Grasses where player can rest
+  for(var i=0;i<6;i++){
+    var bottomGrass1 = createSprite(683,height-50-(i*400),width,grassHeight);
+    if(i%2===0)//adding road
+    {
+     var road= createSprite(683,height-150-(i*400)-grassHeight,width,300,)
+      road.shapeColor="black";
+    }
+    bottomGrass1.shapeColor = "green";
   }
-}
-
-for(i=1;i<carGroup1.length;i++){
-  if(carGroup1[b].x<0){
-    carGroup1[i].x=width
-  }
-}
+  //To create rows of car
+   for(var i = 0; i < 40; i++){
+     cars = new Car(2);
+     carGroup1.add(cars.spt);
+   }
+  //To create rows of Logs
+    for(var i = 0; i < 40; i++){
+      log = new Log(-3);
+      logGroup1.add(log.spt);
+    }
    
  }
 
 function draw() {
   background("skyblue");
  
- 
+  //Making the cars re-apper
+  for(i=1;i<carGroup1.length;i++) {
+    if(carGroup1[i].x>width)
+    {
+     carGroup1[i].x=0;
+    }
+    if(carGroup1[i].x<0)
+    {
+      carGroup1[i].x=width;
+    }
+  }
+
+  //making the logs reapper
+  for(i=1;i<logGroup1.length;i++){
+    if(logGroup1[i].x<0)
+    {
+    logGroup1[i].x=width;
+    }
+  }
 
   drawSprites();
 }
